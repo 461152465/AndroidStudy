0b9e5b6b5254e1d3180cec97f93651ec7a7675aa
461152465 <461152465@qq.com>
2018-04-07 11:36:35 +0800

    finsiht

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -1,6 +1,12 @@
 package com.example.broadcastbestpractice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver; //在onResume()方法中需要用到
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -20,5 +28,51 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    //OnResume()的时候活动在栈顶，OnPause()的时候活动不在栈顶
+    @Override
+    public void onResume(){
+        super.onResume();
+        //监听发出的广播
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        //如果onPause()则需要取消Receiver的监听，并且设置值为null
+        if(receiver != null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+
+    class ForceOfflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, Intent intent){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try to login again");
+            builder.setCancelable(false); //AlertDialog是不能通过后退键取消的
+            //DialogInterface.OnClickListener是一个接口，其中的onClick是一个抽象方法， 所以构造匿名内部类的时候使用@Override重写方法
+            builder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //销毁所有活动
+                    ActivityCollector.finishAll();
+                    //重新启动LoginActivity活动
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
+
     }
 }
--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.example.broadcastbestpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //findViewById的返回值是View类型，所以需要对应的类型转换
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View v){
+               //使用EditView类的getText()方法以及继承Object的toString()方法将获取的字符串传递给一个String对象
+               String account = accountEdit.getText().toString();
+               String password = passwordEdit.getText().toString();
+               //如果账号是admin，密码是123456,那么登陆成功
+               //对于String类型，判断字符串是否相同使用equals方法
+               if(account.equals("admin") && password.equals("123456")){
+                   Intent intent =  new Intent(LoginActivity.this, MainActivity.class);
+                   startActivity(intent);
+                   finish();
+               }
+               else{
+                   //在Android内部类中使用this表示内部类本身，要表示外部类则使用类名.this
+                   Toast.makeText(LoginActivity.this , "account or password is invalid", Toast.LENGTH_SHORT).show();
+               }
+
+           }
+        });
+
     }
 }
